9e68f3d6e05b1c32fc1acbd3961b732190f4df48
hudamerchant <hudazehra2510@gmail.com>
2019-11-17 18:13:14 +0500

    Updated Sign up Login

--- Hireable/application/controllers/Signup.php
+++ Hireable/application/controllers/Signup.php
@@ -8,64 +8,78 @@ class Signup extends MY_Controller
         parent::__construct();
     }
     public function index(){
-        $data['view'] = 'Signup';
-        $data['page_title'] = 'Signup';
-
-        if(isset($_POST['submit'])){
-            $this->form_validation->set_rules('name', 'name', 'required');
-            $this->form_validation->set_rules('dob', 'dob', 'required');
-            $this->form_validation->set_rules('gender', 'gender', 'required');
-            $this->form_validation->set_rules('email', 'email', 'required|is_unique[users.email]|valid_email');
-            $this->form_validation->set_rules('password', 'password', 'required|min_length[8]');
-            $this->form_validation->set_rules('role', 'role', 'required');
-
-            if($this->form_validation->run() == True) {
-            
-                $name           = $this->input->post('name');
-                $dob            = $this->input->post('dob');
-                $role           = $this->input->post('role');
-                $gender         = $this->input->post('gender');
-                $email          = $this->input->post('email');
-                $password       = $this->input->post('password');
-                $re_password    = $this->input->post('re_password');
-
-                if($password == $re_password)
-                {
-                    $hash = password_hash($password,PASSWORD_DEFAULT);
-                
-
-                    $this->load->model('Users');
-                    $data = [
-                        'name' => $name,
-                        'dob' => $dob,
-                        'gender' => $gender,
-                        'email' => $email,
-                        'password' => $hash,
-                        'role_id' => $role
-                    ];
-                    
-                    $this->Users->insertRecord($data);
-                    $this->session->set_flashdata("status","Your account has been created successfully!");
-                    $this->session->set_userdata('logged_in' , True );
-
-                    redirect(site_url('Login'));
-                }
-                else
-                {                    
-                    $data['error']      = 'Incorrect Password';
-                    $this->load->view('layout',$data);
-                    
-                }
-
-                
-
+        if($this->session->userdata('logged_in')){
+            $this->load->model('Users');
+            $where  = [ 'email' => $this->session->userdata('user_info') ];
+            $user   = $this->Users->getData($where)->row();
+            if($user->role_id == 1)
+            {
+                redirect(site_url('Freelancer'));
             }
-            else{
-                $this->load->view('layout',$data);
+            elseif($user->role_id == 2)
+            {
+                redirect(site_url('Client'));
             }
         }
+        else
+        {
+            $data['view'] = 'Signup';
+            $data['site_title'] = 'Hireable';
+            $data['page_title'] = 'Signup -'.$data['site_title'];
 
-        $this->load->view('layout',$data);
+            if(isset($_POST['submit'])){
+                $this->form_validation->set_rules('name', 'name', 'required');
+                $this->form_validation->set_rules('dob', 'dob', 'required');
+                $this->form_validation->set_rules('gender', 'gender', 'required');
+                $this->form_validation->set_rules('email', 'email', 'required|is_unique[users.email]|valid_email');
+                $this->form_validation->set_rules('password', 'password', 'required|min_length[8]');
+                $this->form_validation->set_rules('role', 'role', 'required');
+
+                if($this->form_validation->run() == True) {
+                
+                    $name           = $this->input->post('name');
+                    $dob            = $this->input->post('dob');
+                    $role           = $this->input->post('role');
+                    $gender         = $this->input->post('gender');
+                    $email          = $this->input->post('email');
+                    $password       = $this->input->post('password');
+                    $re_password    = $this->input->post('re_password');
+
+                    if($password == $re_password)
+                    {
+                        $hash = password_hash($password,PASSWORD_DEFAULT);
+                    
+
+                        $this->load->model('Users');
+                        $data = [
+                            'name' => $name,
+                            'dob' => $dob,
+                            'gender' => $gender,
+                            'email' => $email,
+                            'password' => $hash,
+                            'role_id' => $role
+                        ];
+                        
+                        $this->Users->insertRecord($data);
+                        $this->session->set_flashdata("status","Your account has been created successfully!");
+                        redirect(site_url('Login'));
+                    }
+                    else
+                    {                    
+                        $data['error']      = 'Incorrect Password';
+                        $this->load->view('layout',$data);
+                        
+                    }
+
+                    
+
+                }
+                else{
+                    $this->load->view('layout',$data);
+                }
+            }
+            $this->load->view('layout',$data);
+        }
     }
      
 }
