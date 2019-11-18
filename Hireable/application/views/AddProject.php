0eb060dd8e450dbb7f10f8058e44b5a33bb55891
Rida-Jawwad <ridajawad999@gmail.com>
2019-11-18 19:00:26 +0500

    Search and add project page and formatting

---
+++ Hireable/application/views/AddProject.php
@@ -0,0 +1,32 @@
+<section id="content">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-9 col-md-12 col-xs-12">
+                <div class="add-resume box">
+                    <form class="form-ad">
+                        <h3>Add new project</h3>
+                        <div class="form-group">
+                            <label class="control-label">Project title</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Project Description</label>
+                            <textarea class="form-control" rows="7"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <div class="button-group">
+                                <div class="action-buttons">
+                                    <div class="upload-button">
+                                        <button class="btn btn-common">Add</button>
+                                        <input id="cover_img_file_2" type="file">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ Hireable/application/views/Search.php
@@ -0,0 +1,77 @@
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="inner-header">
+                    <h3>Search</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-lg-8 col-md-12 col-xs-12">
+    <div class="job-alerts-item candidates">
+        <!-- <h3 class="alerts-title">Manage Resumes</h3> -->
+        <div class="manager-resumes-item">
+            <div class="manager-content">
+                <a href="resume.html"><img class="resume-thumb" src="<?php echo base_url(); ?>assets/img/jobs/avatar-1.jpg" alt=""></a>
+                <div class="manager-info">
+                    <div class="manager-name">
+                        <h4><a href="#">Zane Joyner</a></h4>
+                        <h5>Front-end developer</h5>
+                    </div>
+                    <div class="manager-meta">
+                        <span class="location"><i class="lni-map-marker"></i> Cupertino, CA, USA</span>
+                        <span class="rate"><i class="lni-alarm-clock"></i> $55 per hour</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="manager-resumes-item">
+            <div class="manager-content">
+                <a href="resume.html"><img class="resume-thumb" src="<?php echo base_url(); ?>assets/img/jobs/avatar-1.jpg" alt=""></a>
+                <div class="manager-info">
+                    <div class="manager-name">
+                        <h4><a href="#">Zane Joyner</a></h4>
+                        <h5>Front-end developer</h5>
+                    </div>
+                    <div class="manager-meta">
+                        <span class="location"><i class="lni-map-marker"></i> Cupertino, CA, USA</span>
+                        <span class="rate"><i class="lni-alarm-clock"></i> $55 per hour</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="manager-resumes-item">
+            <div class="manager-content">
+                <a href="resume.html"><img class="resume-thumb" src="<?php echo base_url(); ?>assets/img/jobs/avatar-1.jpg" alt=""></a>
+                <div class="manager-info">
+                    <div class="manager-name">
+                        <h4><a href="#">Zane Joyner</a></h4>
+                        <h5>Front-end developer</h5>
+                    </div>
+                    <div class="manager-meta">
+                        <span class="location"><i class="lni-map-marker"></i> Cupertino, CA, USA</span>
+                        <span class="rate"><i class="lni-alarm-clock"></i> $55 per hour</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="manager-resumes-item">
+            <div class="manager-content">
+                <a href="resume.html"><img class="resume-thumb" src="<?php echo base_url(); ?>assets/img/jobs/avatar-1.jpg" alt=""></a>
+                <div class="manager-info">
+                    <div class="manager-name">
+                        <h4><a href="#">Zane Joyner</a></h4>
+                        <h5>Front-end developer</h5>
+                    </div>
+                    <div class="manager-meta">
+                        <span class="location"><i class="lni-map-marker"></i> Cupertino, CA, USA</span>
+                        <span class="rate"><i class="lni-alarm-clock"></i> $55 per hour</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
