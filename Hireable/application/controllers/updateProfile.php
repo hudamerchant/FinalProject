be39fae722e2e4ffdd6bf9cf86d20d4e81f2f944
hudamerchant <hudazehra2510@gmail.com>
2019-11-18 21:42:36 +0500

    kuch bhi

--- Hireable/application/controllers/updateProfile.php
+++ Hireable/application/controllers/updateProfile.php
@@ -2,7 +2,16 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     class updateProfile extends MY_Controller{
+        function __construct(){
+        parent::__construct();
+    }
         public function index(){
+            if($this->session->userdata('logged_in')){
+                $this->load->model('Users');
+                $where  = [ 'email' => $this->session->userdata('user_info') ];
+                $user   = $this->Users->getData($where)->row();
+                redirect(site_url('updateProfile'));
+            }
             $data['view'] = 'updateProfile';
             $data['site_title'] = 'Hireable';
             $data['page_title'] = 'Update -'.$data['site_title'];                    
