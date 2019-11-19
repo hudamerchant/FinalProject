a695c0462e6c5111cfdecc42b7c2ee072c0ee38c
Rida-Jawwad <ridajawad999@gmail.com>
2019-11-20 00:47:55 +0500

    AddProject validations

--- Hireable/application/controllers/AddProject.php
+++ Hireable/application/controllers/AddProject.php
@@ -3,17 +3,58 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     class AddProject extends MY_Controller{
-        public function index(){
-            $data['view'] = 'AddProject';
-            $data['site_title'] = 'Hireable';
-            $data['page_title'] = 'Add project -'.$data['site_title'];  
-            
-            //loading database table categories
-            $this->load->model('Categories');
-            $categories         = $this->Categories->getData()->result();
-            $data['categories'] = $categories;
-            $this->load->view('layout',$data);
+        function __construct(){
+            parent::__construct();
         }
+        public function index(){
+            $this->load->model('Users');
+            if($this->session->userdata('logged_in')){
+                $where  = [ 'email' => $this->session->userdata('user_info') ];
+                $user   = $this->Users->getData($where)->row();
+                if($user->role_id == 1)
+                {       
+                    redirect(site_url('Freelancer'));
+                    
+                }
+                elseif($user->role_id == 2)
+                {
+                    $data['view'] = 'AddProject';
+                    $data['site_title'] = 'Hireable';
+                    $data['page_title'] = 'Add project - '.$data['site_title']; 
+                    //$this->load->view('layout',$data);                   
+                    //loading database table categories
+                    $this->load->model('Categories');
+                    $categories         = $this->Categories->getData()->result();
+                    $data['categories'] = $categories;
+                    
+
+                    if(isset($_POST['submit'])){
+                        $this->form_validation->set_rules('project-title', 'project title', 'required');
+                        $this->form_validation->set_rules('project-description', 'project description', 'required');
+                        $this->form_validation->set_rules('categories[]', 'skills', 'required');
+                        // $this->form_validation->set_rules('email', 'e-mail', 'required|valid_email');
+                        if($this->form_validation->run() == True) {
+                            $project_title = $this->input->post('project-title');
+                            $project_description = $this->input->post('project-description');
+                            
+                        }
+                        else{
+                            $this->load->view('layout',$data);
+
+                        }
+                    }
+                    else{
+                        $this->load->view('layout',$data);
+                    }
+                }
+            }
+            else
+            {
+                redirect(site_url('Login'));
+            }
+        }
+            
+        
     }
 
 ?>
--- Hireable/application/views/AddProject.php
+++ Hireable/application/views/AddProject.php
@@ -3,15 +3,17 @@
         <div class="row justify-content-center">
             <div class="col-lg-9 col-md-12 col-xs-12">
                 <div class="add-resume box">
-                    <form class="form-ad">
+                    <form class="form-ad" method="post">
                         <h3>Add new project</h3>
                         <div class="form-group">
                             <label class="control-label">Project title</label>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="project-title">
+                            <?php echo form_error('project-title') ?>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Project Description</label>
-                            <textarea class="form-control" rows="7"></textarea>
+                            <textarea class="form-control" rows="7" name="project-description"></textarea>
+                            <?php echo form_error('project-description') ?>
                         </div>
                         <div class="form-group ">
                             <label class="control-label"></label>   
@@ -21,14 +23,14 @@
                                         <option value="<?php echo $category->category_id ?>"><?php echo $category->category ?></option>            
                                     <?php } ?>
                             </select>
-                            <?php echo form_error('skills') ?>
+                            <?php echo form_error('categories[]') ?>
                         </div>
                         <div class="form-group">
                             <div class="button-group">
                                 <div class="action-buttons">
-                                    <div class="upload-button">
-                                        <button class="btn btn-common">Add</button>
-                                        <input id="cover_img_file_2" type="file">
+                                    <div >
+                                        <!-- <button ></button> -->
+                                        <input class="btn btn-common" type="submit" name="submit" value="Add">
                                     </div>
                                 </div>
                             </div>
