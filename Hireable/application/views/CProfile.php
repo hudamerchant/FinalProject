615f29ebf1f5f1e41b33dc99370d23759d6b9fe9
AymenAmmar <ayammar448@gmail.com>
2019-11-20 02:36:38 -0800

    Client and Freelancer profile

--- Hireable/application/views/CProfile.php
+++ Hireable/application/views/CProfile.php
@@ -14,7 +14,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-4 col-xs-12">
                 <div class="right-sideabr">
-                    <h4>Manage Account</h4>
+                <h4>Manage Account<a href='<?php echo site_url('updateCProfile') ?>'> Edit </a></h4>
                     <ul class="list-item">
                         <li><?php echo $client_info->name ?></li>
                         <li><?php echo $client_info->dob ?></li>
@@ -22,28 +22,23 @@
                         <li><?php echo $client_info->email ?></li>
                     </ul> 
                     </div>
-                    <!-- <div>
-                        <h6><b>*Please add your reviews here</b></h6>
-                    <form method="POST">
-                             <div class="form-group">
-                             <label>REVIEWS</label>
-                             <input type="text" class="form-control"> -->
-                             <!-- <label>REVIEWS:</label>
-                             <textarea id="comments" rows="6" cols="30">
-                             </textarea>
-                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form> 
-                    </div> --> 
-             
             </div>
             <div class="col-lg-8 col-md-8 col-xs-12">
                 <div class="inner-box my-resume">
                     <div class="author-resume">
-                        <div class="thumb">
-                            <img src="<?php echo base_url(); ?>assets/img/testimonial/img4.png" alt="">
+                    <div class="col-md-3 d-inline-block">
+                         <div class="user-pic">
+                         <img src="<?php echo base_url(); ?>assets/img/dp.png" class="img-thumbnail" alt="">
+                         <a href="#"  class="btn btn-common mt-2 ml-2">Upload</a>
+                         </div>
                         </div>
-                        <div class="author-info">
+                        <!-- <form action="" method="POST" enctype='multipart/form-data'>
+                        <div class="form-group">
+                          <label for="profile">Profile</label>
+                          <input class="form-control" type="file" name="profile" id="profile">
+                        </div> 
+                        </form> -->
+                        <div class="author-info d-inline-block ml-3">
                             <h3><b><?php echo $client_info->name ?></b></h3>
                             <p class="sub-title">UI/UX Designer</p>
                             <p><span class="address"><i class="lni-map-marker"></i>Mahattan, NYC, USA</span> <span><i class="ti-phone"></i>(+01) 211-123-5678</span></p>
@@ -54,21 +49,13 @@
                                 <a href="#" class="linkedin"><i class="lni-linkedin-fill"></i></a>
                             </div>
                         </div>
-                        <form action="" method="POST" enctype='multipart/form-data'>
-                            <div class="form-group col-md-5">
-                                <!-- <label for="profile">Profile Photo</label> -->
-                                <input class="form-control" type="file" name="profile" id="profile">
-
                                 </div>
                         
-                            <div class="about-me item">
-                                <h3>About Me</h3>
-                                <p>Nullam semper erat arcu, ac tincidunt sem venenatis vel. Curabitur a dolor ac ligula fermentum eusmod ac ullamcorper nulla. Integer blandit uitricies aliquam. Pellentesque quis dui varius, dapibus vilit id, ipsum. Morbi ac eros feugiat, lacinia elit ut, elementum turpis. Curabitur justo sapien, tempus sit amet ruturm eu, commodo eu lacus. Morbi in ligula nibh. Maecenas ut mi at odio hendririt eleif end tempor vitae augue. Fusce eget arcu et nibh dapibus maximus consectetur in est. Sed iaculis Luctus nibh sed veneatis. </p>
-                            </div>
+                            
 
                             <form class="form-ad">
                         <div class="form-group">
-                            <label class="control-label pt-5"><b class="text-dark">Please add your reviews here</b></label>
+                            <label class="control-label pt-3"><h6>Please add your reviews here</h6></label>
                             <textarea class="form-control" rows="5"></textarea>
                         </div>
                         <div class="form-group">
@@ -76,7 +63,6 @@
                                 <div class="action-buttons">
                                     <div class="upload-button">
                                         <button class="btn btn-common">Submit</button>
-                                        <!-- <input id="cover_img_file_2" type="file"> -->
                                     </div>
                                 </div>
                             </div>
--- Hireable/application/views/FProfile.php
+++ Hireable/application/views/FProfile.php
@@ -24,27 +24,25 @@
                     </ul>
                 </div>
                 <div>
-                <!-- <h6><b>*Please add your reviews here</b></h6> -->
-                    <!-- <form method="POST">
-                             <div class="form-group col-xs-12">
-                              <label>REVIEWS</label>
-                             <input type="text" class="form-control"> -->
-                             <!-- <label>REVIEWS:</label>
-                             <textarea id="comments" rows="6" cols="30">
-                             </textarea>
-                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>  --> 
                 </div>
                     </div>
             <div class="col-lg-8 col-md-8 col-xs-12">
                 <div class="inner-box my-resume">
                     <div class="author-resume">
-                        <div class="thumb">
-                            <img src="<?php echo base_url(); ?>assets/img/testimonial/img4.png" alt="">
+                    
+                        <div class="col-md-3 d-inline-block">
+                         <div class="user-pic">
+                         <img src="<?php echo base_url();?>assets/img/dp.png" class="img-thumbnail" alt="">
+                         <a href="#"  class="btn btn-common mt-2 ml-2">Upload</a>
+                         </div>
                         </div>
-
-                        <div class="author-info">
+                        <!-- <form action="" method="POST" enctype='multipart/form-data'>
+                        <div class="form-group">
+                          <label for="profile">Profile</label>
+                          <input class="form-control" type="file" name="profile" id="profile">
+                        </div> 
+                        </form> -->
+                        <div class="author-info d-inline-block ml-3">
                             <h3><b><?php echo $freelancer_info->name ?></b></h3>
                             <p class="sub-title">UI/UX Designer</p>
                             <p><span class="address"><i class="lni-map-marker"></i>Mahattan, NYC, USA</span> <span><i class="ti-phone"></i>(+01) 211-123-5678</span></p>
@@ -54,24 +52,18 @@
                                 <a href="#" class="google"><i class="lni-google-plus"></i></a>
                                 <a href="#" class="linkedin"><i class="lni-linkedin-fill"></i></a>
                             </div>
+                            
                         </div>
-
-                        <form action="" method="POST" enctype='multipart/form-data'>
-                            <div class="form-group col-md-5">
-                                <!-- <label for="profile">Profile Photo</label> -->
-                                <input class="form-control" type="file" name="profile" id="profile">
-
-                            </div>
-                        </form>
                     </div>
                     <div class="about-me item">
                         <h3>About Me</h3>
                         <p>Nullam semper erat arcu, ac tincidunt sem venenatis vel. Curabitur a dolor ac ligula fermentum eusmod ac ullamcorper nulla. Integer blandit uitricies aliquam. Pellentesque quis dui varius, dapibus vilit id, ipsum. Morbi ac eros feugiat, lacinia elit ut, elementum turpis. Curabitur justo sapien, tempus sit amet ruturm eu, commodo eu lacus. Morbi in ligula nibh. Maecenas ut mi at odio hendririt eleif end tempor vitae augue. Fusce eget arcu et nibh dapibus maximus consectetur in est. Sed iaculis Luctus nibh sed veneatis. </p>
                     </div>
                     <form class="form-ad">
-                        
+
+
                         <div class="form-group">
-                            <label class="control-label pt-5"><b class="text-dark">Please add your reviews here</b></label>
+                            <label class="control-label pt-3"><h6>Please add your reviews here</h6></label>
                             <textarea class="form-control" rows="5"></textarea>
                         </div>
                         <div class="form-group">
@@ -79,7 +71,6 @@
                                 <div class="action-buttons">
                                     <div class="upload-button">
                                         <button class="btn btn-common">Submit</button>
-                                        <!-- <input id="cover_img_file_2" type="file"> -->
                                     </div>
                                 </div>
                             </div>
