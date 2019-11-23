8e67e7f40b6e78aa632f3d72e4a1d69fa817f4b5
AymenAmmar <ayammar448@gmail.com>
2019-11-23 12:05:16 -0800

    db review

--- Hireable/application/controllers/AddFreelancerReview.php
+++ Hireable/application/controllers/AddFreelancerReview.php
@@ -37,6 +37,73 @@
                 return redirect(site_url('Login'));
             }
         }
+
+                $where  = [ 'email' => $this->session->userdata('user_info') ];
+                $user   = $this->Users->getData($where)->row();
+
+                if($user->role_id == 2)
+                {       
+                    redirect(site_url('Client'));                    
+                }
+                elseif($user->role_id == 1)
+                {
+                    $this->data['view'] = 'AddFreelancerReview';
+                    $this->data['site_title'] = 'Hireable';
+                    $this->data['page_title'] = 'AddFreelancerReview -'.$this->data['site_title']; 
+                  
+                    $this->data['freelancer_info'] = $user;
+                    
+                    //loading database table client_rating
+                    $this->load->model('Comment');
+                    // $reviews = $this->Comment->getData()->result();
+                    // // $data['review'] =$Comment; 
+                    //  // var_dump($this->data);die;
+                    //  $arr = [];
+                    // foreach ($reviews as $review) {
+                    //   //  var_dump($review) ;die;
+                    //     $arr[] = $review->review;
+                    // }
+                     //var_dump($review);die;
+    
+    
+                    //$this->data['comment'] = $arr;
+                    //var_dump($this->data['comment'] = $arr);die;
+                    if(isset($_POST['submit']))
+                    
+                    {
+                        // var_dump($_POST['submit']);die;
+                        $this->form_validation->set_rules('review', 'review', 'required');
+                    
+                    // var_dump($review);die;
+                    if($this->form_validation->run() == True)
+                    {
+                        $review = $this->input->post('review');
+                        // var_dump($review);die;
+                    
+                        $reviewData = [
+                            'review' => $review,
+                            'user_id' => $user->user_id
+                        ];
+
+                    //  var_dump($reviewData);die;
+
+                        $this->Comment->insertRecord($reviewData);
+                        $this->session->set_flashdata("reviewInserted","Review inserted successfully!");
+                    // $this->load->view('layout', $this->data);
+                    
+                    }
+                   
+                    else {
+                        $this->load->view('layout', $this->data);
+                    }
+                }
+                $this->load->view('layout', $this->data);
+                } 
+            }
+         else {
+             redirect(site_url('Login'));
+    }
+}
     }
 
 ?>
--- Hireable/application/views/AddFreelancerReview.php
+++ Hireable/application/views/AddFreelancerReview.php
@@ -1,22 +1,70 @@
 <div class="container">
     <div class="row">
         <div class="col-md-11">
-        <form class="form-ad">
+        <form class="form-ad" method="post">
                         <div class="form-group">
                             <label class="control-label pt-3"><h6>Please add your reviews here</h6></label>
-                            <textarea class="form-control" rows="5"></textarea>
+                            <textarea class="form-control" rows="5" name="review"></textarea>
                         </div>
                         <div class="form-group">
                             <div class="button-group">
                                 <div class="action-buttons">
+                                <!-- <div class="action-buttons"> -->
                                     <div class="upload-button">
-                                        <button class="btn btn-common">Submit</button>
+                                        
+
+                                            <input class="btn btn-common" type="submit" name="submit" value="Submit">
+                                        </form>
+                                       
+                                        <form action="" method="post" enctype='multipart/form-data'>
+                                            <div class="form-group col-md-5">
+                                                <div>
+                                                    <?php
+                                                    if (isset($_SESSION['reviewInserted'])) {
+                                                        ?>
+                                                        <p class="alert alert-success mt-3"><?php echo $this->session->flashdata("reviewInserted"); ?></p>
+
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                </div>
+                                                <!-- <label for="profile">Profile Photo</label> -->
+                                                <input class="form-control" type="file" name="profile" id="profile">
+
+                                            </div>
+                                        </form>
+                                        <h6><b class="mt-5 text-dark">REVIEWS</b></h6>
+                                        <?php //foreach($comment as $comments)
+                                                    //{
+                                                     ?>
+                                        <div class="manager-resumes-item">
+                                            <div class="manager-content">
+                                                <a href="resume.html"><img class="resume-thumb" src="<?php echo base_url(); ?>assets/img/jobs/avatar-1.png" alt=""></a>
+                                                <div class="manager-info">
+                                                    <div class="manager-name">
+                                                        <h4><a href="#">Client</a></h4>
+                                                        <h5>client@example.com</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="item-body">
+                                                <div class="content">
+                                                    <b>Review</b>
+                                                    
+                                                    <P><?php //echo $comments?>
+                                                    
+                                                </p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php //} ?>
                                     </div>
-                                </div>
+                                <!-- </div> -->
                             </div>
                         </div>
 
-                    </form>
+                   
     
         </div>
     </div>
