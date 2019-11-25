a1694ee0609b327c84a5d2cf3a4a219f8b2d9254
Rida-Jawwad <ridajawad999@gmail.com>
2019-11-25 19:51:01 +0500

    delete project, fetching project where deleted at is null, freelancer listing

---
+++ Hireable/application/controllers/DeleteProject.php
@@ -0,0 +1,53 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class DeleteProject extends MY_Controller
+    {
+        public function __construct()
+        {
+            parent::__construct();
+        }
+        public function index($project_id = false)
+        {
+            $this->data['view']       = 'DeleteProject';
+            $this->data['site_title'] = 'Hireable';
+            $this->data['page_title'] = 'Delete Project - '.$this->data['site_title'];
+            $this->load->model('Users');
+            if ($this->session->userdata('logged_in')) {
+                $where  = [ 'email' => $this->session->userdata('user_info') ];
+                $user   = $this->Users->getData($where)->row();
+                if ($user->role_id == 1) 
+                {
+                    return redirect(site_url('Freelancer'));
+                } 
+                elseif ($user->role_id == 2) 
+                {
+                    if(!$user->updated_profile)
+                    {
+                        return redirect(site_url('updateCProfile'));
+                    }
+                    else
+                    {
+                        if ($project_id) {
+                            $this->load->model('Projects');
+                            $dataToUpdate = [
+                                'deleted_at' => date("Y-m-d H:i:s")
+                            ];
+                            $whereProjectID = [
+                                'project_id' => $project_id
+                            ];
+                            $this->Projects->updateData($dataToUpdate,$whereProjectID);
+                            return redirect(site_url('Client'));
+                        }
+                        return $this->load->view('layout',$this->data);
+
+        
+                    }
+                }
+            }
+            else
+            {
+                return redirect(site_url('Login'));
+            }
+        }
+    }
