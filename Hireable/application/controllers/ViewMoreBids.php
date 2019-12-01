4d3b613fcf55297ce9082b1a3b935e4e0a57dc7c
Rida-Jawwad <ridajawad999@gmail.com>
2019-12-01 18:29:39 +0500

    View bids page(project title and categories, freelancer categories)

--- Hireable/application/controllers/ViewMoreBids.php
+++ Hireable/application/controllers/ViewMoreBids.php
@@ -37,20 +37,47 @@
                             foreach ($projects as $project) {
                                 $this->data['projects'][$count]['project_id'] = $project->project_id;
                                 
+                                
                             }
                             
                             if($project_apply_id){
-                                
+                                $whereAppliedProjectId = [
+                                    'project_id' => $project_apply_id
+                                ];
+                                $appliedIdProject = $this->Projects->getData('DESC',$whereAppliedProjectId)->result();
+                                // $this->data['projects'][$count]['title'] = $appliedIdProject->project_title;
+                                foreach($appliedIdProject as $appliedIdProjectTitle){
+                                    $this->data['project_title'] = $appliedIdProjectTitle->project_title;
+                                    // var_dump($appliedIdProjectTitle->project_title);die;
+
+                                }
                                 $this->data['project_apply_id'] = $project_apply_id;
+                                $category_id = [];
+                                $this->load->model('ProjectCategories');
+                                $projectCategoryWhere = [
+                                    'project_id' => $project_apply_id
+                                ];
+                                $categoryVariable = $this->ProjectCategories->getData('DESC' ,$projectCategoryWhere)->result();
+                                foreach ($categoryVariable as $categoryVariable2) {
+                                    $category_id[] =    $categoryVariable2->category_id;
+                                }
+                                $this->load->model('Categories');
+                                $categories = $this->Categories->getDataWhereIn("category_id",$category_id)->result(); 
+                                foreach ($categories as $category) {
+                                    $this->data['projects'][$count]['categories'][] = $category->category;
+                                }
+
                                 $this->load->model('ProjectBid');
                                 $whereBid = [
                                     'project_id' => $project_apply_id
                                 ];
+                                // var_dump($project_apply_id);die;
                                 $project_bids = $this->ProjectBid->getData('DESC' ,$whereBid)->result();
 
                                 if($project_bids){
                                     $this->session->set_flashdata("projectsBidsPresent",true);
                                     foreach ($project_bids as $project_bid) {
+                                        $category_id = [];
                                         $this->data['data_project_bids'][$count]['project_user_id']= $user->user_id;
                                         $this->data['data_project_bids'][$count]['bid_user_id']= $project_bid->user_id;
                                         $this->data['data_project_bids'][$count]['bid_project_id']= $project_bid->project_id;
@@ -66,6 +93,22 @@
                                         if($userData->profile_pic != ''){
                                             $this->data['data_project_bids'][$count]['profile_pic'] = $this->data['image_path'].$userData->profile_pic;
                                         }
+
+                                        $this->load->model('FCategories');
+                                        $freelancerCategoriesWhere = [
+                                            'user_id' => $userData->user_id
+                                        ];
+                                        $categoryVariable = $this->FCategories->getData('DESC',$freelancerCategoriesWhere)->result();
+                                        foreach ($categoryVariable as $categoryVariable2) {
+                                            $category_id[] =    $categoryVariable2->category_id;
+                                        }
+                                        $this->load->model('Categories');
+                                        $categories = $this->Categories->getDataWhereIn("category_id",$category_id)->result(); 
+                                        foreach ($categories as $category) {
+                                            $this->data['data_project_bids'][$count]['categories'][] = $category->category;
+                                            // var_dump($category->category);
+                                        }
+
                                         $count++;                                        
                                     }
                                 }                              
--- Hireable/application/views/ViewMoreBids.php
+++ Hireable/application/views/ViewMoreBids.php
@@ -9,18 +9,53 @@
         </div>
     </div>
 </div>
+
 <?php
 if (isset($_SESSION['projectsBidsPresent'])) {
     if($data_project_bids){
         // var_dump($data_project_bids);die;
     foreach ($data_project_bids as $project_bid) {
         ?>
+
         <section class="job-detail section dashboard-section">
             <div class="container">
                 <div class="row justify-content-between">
-                    <div class="col-lg-8 col-md-12 col-xs-12">
-                        <div class="content-area">
+                    <div class="col-lg-8 col-md-12 col-xs-12">                        
+                        <div class="content-area"> 
+                            <div class="content-area">
+                                <div class="manager-info">
+                                <h5 class="freelancer-details-h6">Project Details</h5>
+                                    <h6><b>Project Title</b></h6>
+                                    <p><?php echo $project_title ?></p>
+                                    <h6><b>Project Skills</b></h6>
+                                    <div class="manager-resumes-item client-dashboard-manager">
+                                        <div class="item-body client-dashboard-item">
+                                            <div class="resume-skills client-dashboard-resume">
+                                                <?php 
+                                                    foreach($projects as $project){
+                                                        ?>
+                                                            <div class="tag-list">
+                                                                <?php foreach ($project['categories'] as  $category) 
+                                                                {
+                                                                ?>
+                                                                    <span><?php echo $category ?></span>
+                                                                <?php 
+                                                                }  
+                                                                ?>
+                                                            </div>
+                                                        <?php
+                                                    }
+                                                ?>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+              
                             <div class="manager-resumes-item">
+                                
+                                <h5 class="freelancer-details-h6">Freelancer Details</h5>
                                 <div class="manager-content">
                                 <?php 
                                     if(isset($project_bid['profile_pic'])){
@@ -37,13 +72,31 @@ if (isset($_SESSION['projectsBidsPresent'])) {
                                     <div class="manager-info">
                                         <div class="manager-name">
                                             <h4 class="view-bids-h4"><a href="#"><?php echo $project_bid['bid_username'] ?></a></h4>
-                                            <h5><?php echo $project_bid['bid_email'] ?></h5>
+                                            <h5><?php echo $project_bid['bid_email'] ?></h5>              
+                                        </div>
+                                        <div class="manager-resumes-item client-dashboard-manager">
+                                                <div class="item-body client-dashboard-item">
+                                                    <div class="resume-skills client-dashboard-resume">
+                                                    
+                                                    <div class="tag-list freelancer-tag-skills">
+                                                        
+                                                        <?php foreach ($project_bid['categories'] as  $category) 
+                                                        {
+                                                        ?>
+                                                            <span><?php echo $category ?></span>
+                                                        <?php 
+                                                        }  
+                                                        ?>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        
+                                        <div class="manager-meta view-bids-button">
+                                            <span><a href="<?php echo site_url('HireFreelancer/index/'.$project_bid['bid_user_id'].'/'.$project_bid['bid_project_id'].'/'.$project_bid['project_user_id']) ?>" class="btn btn-common view-bids-anchor">Hire</a></span>
                                         </div>
                                         <div class="manager-meta view-bids-button">
                                             <span><a href="<?php echo site_url('FreelancerProfileForClients/index/'.$project_bid['bid_user_id']) ?>" class="btn btn-common view-bids-anchor">View Profile</a></span>
-                                        </div>
-                                        <div class="manager-meta view-bids-button">
-                                            <span><a href="<?php echo site_url('HireFreelancer/index/'.$project_bid['bid_user_id'].'/'.$project_bid['bid_project_id'].'/'.$project_bid['project_user_id']) ?>" class="btn btn-common view-bids-anchor">Hire</a></span>
                                         </div>
                                     </div>
                                 </div>
